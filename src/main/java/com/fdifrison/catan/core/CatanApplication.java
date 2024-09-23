2d6beca82f5c2ac22011de37d79693c1dc925885
fdifrison <ing.giovanni.frison@gmail.com>
2024-09-23 12:08:04 +0200

    Project init

---
+++ src/main/java/com/fdifrison/catan/core/CatanApplication.java
@@ -0,0 +1,13 @@
+package com.fdifrison.catan.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fdifrison/catan/core/CatanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdifrison.catan.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

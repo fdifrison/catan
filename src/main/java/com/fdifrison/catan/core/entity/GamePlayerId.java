e15fd0bf3152b186bbd966a960439a032829fcd1
fdifrison <ing.giovanni.frison@gmail.com>
2024-09-25 16:20:26 +0200

    spotless:apply

--- src/main/java/com/fdifrison/catan/core/entity/GamePlayerId.java
+++ src/main/java/com/fdifrison/catan/core/entity/GamePlayerId.java
@@ -2,15 +2,15 @@ package com.fdifrison.catan.core.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import org.hibernate.Hibernate;
-
 import java.io.Serial;
 import java.util.Objects;
+import org.hibernate.Hibernate;
 
 @Embeddable
 public class GamePlayerId implements java.io.Serializable {
     @Serial
     private static final long serialVersionUID = -2872401453343906886L;
+
     @Column(name = "player_id", nullable = false)
     private Long playerId;
 
@@ -38,13 +38,11 @@ public class GamePlayerId implements java.io.Serializable {
         if (this == o) return true;
         if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
         GamePlayerId entity = (GamePlayerId) o;
-        return Objects.equals(this.gameId, entity.gameId) &&
-                Objects.equals(this.playerId, entity.playerId);
+        return Objects.equals(this.gameId, entity.gameId) && Objects.equals(this.playerId, entity.playerId);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(gameId, playerId);
     }
-
 }

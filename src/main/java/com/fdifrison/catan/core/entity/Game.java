f26adcf47b724b9aa7ef40dc0323013baf684f0a
fdifrison <ing.giovanni.frison@gmail.com>
2024-09-25 16:15:20 +0200

    Entities

---
+++ src/main/java/com/fdifrison/catan/core/entity/Game.java
@@ -0,0 +1,59 @@
+package com.fdifrison.catan.core.entity;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "game")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "game_id_gen")
+    @SequenceGenerator(name = "game_id_gen", sequenceName = "game_id_seq", allocationSize = 25)
+    private Long id;
+
+    @Column(name = "start_timestamp", nullable = false)
+    private Instant startTimestamp;
+
+    @Column(name = "end_timestamp")
+    private Instant endTimestamp;
+
+    @OneToMany(mappedBy = "game")
+    private Set<GamePlayer> gamePlayers = new LinkedHashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getStartTimestamp() {
+        return startTimestamp;
+    }
+
+    public void setStartTimestamp(Instant startTimestamp) {
+        this.startTimestamp = startTimestamp;
+    }
+
+    public Instant getEndTimestamp() {
+        return endTimestamp;
+    }
+
+    public void setEndTimestamp(Instant endTimestamp) {
+        this.endTimestamp = endTimestamp;
+    }
+
+    public Set<GamePlayer> getGamePlayers() {
+        return gamePlayers;
+    }
+
+    public void setGamePlayers(Set<GamePlayer> gamePlayers) {
+        this.gamePlayers = gamePlayers;
+    }
+
+}
---
+++ src/main/java/com/fdifrison/catan/core/entity/GamePlayerId.java
@@ -0,0 +1,50 @@
+package com.fdifrison.catan.core.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import org.hibernate.Hibernate;
+
+import java.io.Serial;
+import java.util.Objects;
+
+@Embeddable
+public class GamePlayerId implements java.io.Serializable {
+    @Serial
+    private static final long serialVersionUID = -2872401453343906886L;
+    @Column(name = "player_id", nullable = false)
+    private Long playerId;
+
+    @Column(name = "game_id", nullable = false)
+    private Long gameId;
+
+    public Long getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(Long playerId) {
+        this.playerId = playerId;
+    }
+
+    public Long getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(Long gameId) {
+        this.gameId = gameId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        GamePlayerId entity = (GamePlayerId) o;
+        return Objects.equals(this.gameId, entity.gameId) &&
+                Objects.equals(this.playerId, entity.playerId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gameId, playerId);
+    }
+
+}

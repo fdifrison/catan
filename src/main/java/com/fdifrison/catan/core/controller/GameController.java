bf622cc39f34bf44386804d291a70bb8c743d42f
fdifrison <ing.giovanni.frison@gmail.com>
2024-09-25 16:21:42 +0200

    Rest Controllers

---
+++ src/main/java/com/fdifrison/catan/core/controller/GameController.java
@@ -0,0 +1,17 @@
+package com.fdifrison.catan.core.controller;
+
+import com.fdifrison.catan.core.service.GameService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("game")
+public class GameController {
+
+    private final GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+}

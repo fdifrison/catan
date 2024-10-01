7898e67a125a383f509f795eb73424dea8eb8ea5
fdifrison <ing.giovanni.frison@gmail.com>
2024-10-01 12:57:05 +0200

    Fix statistics endpoints

--- src/main/java/com/fdifrison/catan/core/controller/StatisticsController.java
+++ src/main/java/com/fdifrison/catan/core/controller/StatisticsController.java
@@ -2,10 +2,7 @@ package com.fdifrison.catan.core.controller;
 
 import com.fdifrison.catan.core.dto.DiceDashboardDTO;
 import com.fdifrison.catan.core.service.StatisticsService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("statistics")
@@ -17,13 +14,13 @@ public class StatisticsController {
         this.statisticsService = statisticsService;
     }
 
-    @GetMapping("{gameId}/dice-dashboard")
-    public DiceDashboardDTO getGameDiceDashboard(@PathVariable("gameId") long gameId) {
+    @GetMapping("game-dice-dashboard")
+    public DiceDashboardDTO getGameDiceDashboard(@RequestParam("gameId") long gameId) {
         return statisticsService.getGameDiceDashboard(gameId);
     }
 
-    @GetMapping("{playerId}/dice-dashboard")
-    public DiceDashboardDTO getPlayerOverallDiceDashboard(@PathVariable("playerId") long playerId) {
+    @GetMapping("player-dice-dashboard")
+    public DiceDashboardDTO getPlayerOverallDiceDashboard(@RequestParam("playerId") long playerId) {
         return statisticsService.getPlayerOverallDiceDashboard(playerId);
     }
 }

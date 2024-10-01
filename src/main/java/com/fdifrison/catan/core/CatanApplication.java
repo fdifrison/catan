527ed543fe1c56dc473def71b2208cc56f85c2f3
fdifrison <ing.giovanni.frison@gmail.com>
2024-10-01 11:35:21 +0200

    Add sample data for one game at runtime

--- src/main/java/com/fdifrison/catan/core/CatanApplication.java
+++ src/main/java/com/fdifrison/catan/core/CatanApplication.java
@@ -7,8 +7,10 @@ import com.fdifrison.catan.core.service.GameService;
 import com.fdifrison.catan.core.service.PlayerService;
 import com.fdifrison.catan.core.service.StatisticsService;
 import com.fdifrison.catan.core.service.TurnService;
+
 import java.util.List;
 import java.util.Random;
+
 import org.instancio.Instancio;
 import org.springdoc.core.utils.SpringDocUtils;
 import org.springframework.boot.CommandLineRunner;
@@ -35,19 +37,20 @@ public class CatanApplication {
             var p3 = new PlayerScoreDTO(p3Id, 2);
             var newGame = gameService.createNewGame(List.of(p1, p2, p3));
             Random random = new Random();
-            for (int i = 0; i < 100; i++) {
-                var dice = random.nextInt((12 - 2) + 1) + 2;
-                turnService.initTurn(new InitTurnDTO(newGame, p1Id, dice));
+            for (Long p : List.of(p1Id, p2Id, p3Id)) {
+                for (int i = 0; i < 20; i++) {
+                    var dice = random.nextInt((12 - 2) + 1) + 2;
+                    turnService.initTurn(new InitTurnDTO(newGame, p, dice));
+                }
             }
-            statisticsService.getGameDiceDashboard(newGame);
+//            statisticsService.getGameDiceDashboard(newGame);
 
-
-            //            var p1f = new PlayerScoreDTO(p1Id, 3, false, false, 5, 2);
-            //            var p2f = new PlayerScoreDTO(p2Id, 1, false, true, 8, 2);
-            //            var p3f = new PlayerScoreDTO(p3Id, 2, true, false, 12, 2);
-            //            gameService.updateScoreAndEndGame(newGame, List.of(p1f, p2f, p3f));
-            //            var gameRanking = gameService.getGameRanking(newGame);
-            //            gameRanking.forEach(System.out::println);
+            var p1f = new PlayerScoreDTO(p1Id, 3, false, false, 5, 2);
+            var p2f = new PlayerScoreDTO(p2Id, 1, false, true, 8, 2);
+            var p3f = new PlayerScoreDTO(p3Id, 2, true, false, 12, 2);
+            gameService.updateScoreAndEndGame(newGame, List.of(p1f, p2f, p3f));
+//            var gameRanking = gameService.getGameRanking(newGame);
+//            gameRanking.forEach(System.out::println);
         };
     }
 }

eb4a953c7ae1fc6b59a1ba9c1c8d0218114c1e22
fdifrison <ing.giovanni.frison@gmail.com>
2024-10-01 17:22:38 +0200

    Returning id in game and player dto

--- src/main/java/com/fdifrison/catan/core/dto/GameDTO.java
+++ src/main/java/com/fdifrison/catan/core/dto/GameDTO.java
@@ -1,54 +1,7 @@
 package com.fdifrison.catan.core.dto;
 
 import com.fdifrison.catan.core.entity.PlayerScore;
-import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
-public class GameDTO {
-
-    private Integer id;
-
-    private Instant startTimestamp;
-
-    private Instant endTimestamp;
-
-    private List<PlayerScore> playerScores = new ArrayList<>();
-
-    public Instant getEndTimestamp() {
-        return endTimestamp;
-    }
-
-    public GameDTO setEndTimestamp(Instant endTimestamp) {
-        this.endTimestamp = endTimestamp;
-        return this;
-    }
-
-    public @NotNull Integer getId() {
-        return id;
-    }
-
-    public GameDTO setId(@NotNull Integer id) {
-        this.id = id;
-        return this;
-    }
-
-    public List<PlayerScore> getPlayerScores() {
-        return playerScores;
-    }
-
-    public GameDTO setPlayerScores(List<PlayerScore> playerScores) {
-        this.playerScores = playerScores;
-        return this;
-    }
-
-    public @NotNull Instant getStartTimestamp() {
-        return startTimestamp;
-    }
-
-    public GameDTO setStartTimestamp(@NotNull Instant startTimestamp) {
-        this.startTimestamp = startTimestamp;
-        return this;
-    }
-}
+public record GameDTO(long id, Instant startTimestamp, Instant endTimestamp, List<PlayerScore> playerScores) {}

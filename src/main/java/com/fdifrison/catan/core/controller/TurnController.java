8141a0028d5abbcbc15bb8465e2f6fce5f05af28
fdifrison <ing.giovanni.frison@gmail.com>
2024-10-03 00:50:37 +0200

    Reset Turn logic

--- src/main/java/com/fdifrison/catan/core/controller/TurnController.java
+++ src/main/java/com/fdifrison/catan/core/controller/TurnController.java
@@ -1,9 +1,6 @@
 package com.fdifrison.catan.core.controller;
 
-import com.fdifrison.catan.core.dto.EndTurnDTO;
-import com.fdifrison.catan.core.dto.InitTurnDTO;
 import com.fdifrison.catan.core.service.TurnService;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,16 +11,5 @@ public class TurnController {
 
     public TurnController(TurnService turnService) {
         this.turnService = turnService;
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public long newTurn(@RequestBody InitTurnDTO initTurnDTO) {
-        return turnService.initTurn(initTurnDTO);
-    }
-
-    @PutMapping("{id}")
-    public void endTurn(@PathVariable long id, @RequestBody EndTurnDTO endTurnDTO) {
-        turnService.endTurn(id, endTurnDTO);
     }
 }

b7d32eb92f86b1644401f396a33f10b79a67eaf4
fdifrison <ing.giovanni.frison@gmail.com>
2024-10-04 18:39:17 +0200

    Aggregate player statistics

---
+++ src/main/java/com/fdifrison/catan/core/entity/projection/IPlayerAggregateGameStatistics.java
@@ -0,0 +1,12 @@
+package com.fdifrison.catan.core.entity.projection;
+
+public interface IPlayerAggregateGameStatistics{
+    Long getId();
+    Long getDevelopcarddrawn();
+    Long getKnightcardplayed();
+    Long getRoadsbuilt();
+    Long getColoniesbuilt();
+    Long getCitiesbuilt();
+    Boolean getLongestRoad();
+    Boolean getLargestArmy();
+}

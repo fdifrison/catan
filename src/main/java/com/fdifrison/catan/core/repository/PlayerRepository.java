b59d316f01951bec7c1b304e25f83488880ef285
fdifrison <ing.giovanni.frison@gmail.com>
2024-10-05 15:10:43 +0200

    Find player in one shot as playerIDs in

--- src/main/java/com/fdifrison/catan/core/repository/PlayerRepository.java
+++ src/main/java/com/fdifrison/catan/core/repository/PlayerRepository.java
@@ -5,5 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface PlayerRepository extends JpaRepository<Player, Long>, JpaSpecificationExecutor<Player> {}
+public interface PlayerRepository extends JpaRepository<Player, Long>, JpaSpecificationExecutor<Player> {
+    List<Player> findAllByIdIn(List<Long> playerIds);
+}

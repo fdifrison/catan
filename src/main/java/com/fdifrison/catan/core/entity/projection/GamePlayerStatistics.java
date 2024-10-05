6f2e6e357e20c09bdbd400cc3f3b5898f305787f
fdifrison <ing.giovanni.frison@gmail.com>
2024-10-05 14:24:40 +0200

    Game Player statistics by turn

---
+++ src/main/java/com/fdifrison/catan/core/entity/projection/GamePlayerStatistics.java
@@ -0,0 +1,12 @@
+package com.fdifrison.catan.core.entity.projection;
+
+public interface GamePlayerStatistics {
+    Long getId();
+    Long getDevelopCardDrawn();
+    Long getKnightCardPlayed();
+    Long getRoadsBuilt();
+    Long getColoniesBuilt();
+    Long getCitiesBuilt();
+    Boolean getLongestRoad();
+    Boolean getLargestArmy();
+}
--- src/main/java/com/fdifrison/catan/core/service/StatisticsService.java
+++ src/main/java/com/fdifrison/catan/core/service/StatisticsService.java
@@ -1,16 +1,23 @@
 package com.fdifrison.catan.core.service;
 
 import com.fdifrison.catan.core.dto.DiceDashboardDTO;
+import com.fdifrison.catan.core.dto.GameDTO;
+import com.fdifrison.catan.core.dto.mapper.GamePlayerMapper;
 import com.fdifrison.catan.core.repository.TurnRepository;
+import org.springframework.data.util.StreamUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StatisticsService {
 
     private final TurnRepository turnRepository;
+    private final GamePlayerMapper gamePlayerMapper;
 
-    public StatisticsService(TurnRepository turnRepository) {
+    public StatisticsService(TurnRepository turnRepository, GamePlayerMapper gamePlayerMapper) {
         this.turnRepository = turnRepository;
+        this.gamePlayerMapper = gamePlayerMapper;
     }
 
     public DiceDashboardDTO getGameDiceDashboard(long gameId) {
@@ -29,8 +36,25 @@ public class StatisticsService {
         return diceDashboard;
     }
 
-    public void dummy(long gameId) {
-        var playerAggregateGameStatisticsByGameId = turnRepository.findPlayerAggregateGameStatisticsByGameId(gameId);
-        System.out.println();
+    public List<GameDTO.GamePlayerDTO> computeGamePlayerStatistics(long gameId, List<GameDTO.GamePlayerDTO> gamePlayers) {
+        var statistics = turnRepository.computeGamePlayerStatistics(gameId);
+        return StreamUtils.zip(
+                        gamePlayers.stream().sorted(),
+                        statistics.stream(),
+                        gamePlayerMapper::updateDtoWithStatistics)
+                .map(this::computePlayerScore)
+                .toList();
+    }
+
+    private GameDTO.GamePlayerDTO computePlayerScore(GameDTO.GamePlayerDTO player) {
+        var score = player.citiesBuilt() * 2 + (player.coloniesBuilt() - player.citiesBuilt());
+        int scoreWithBonus = player.longestRoad() || player.largestArmy() ? score + 2 : score;
+        return gamePlayerMapper.updateDtoWithScore(player, scoreWithBonus);
+
+    }
+
+    // remove the positioning initial turns from count
+    public long countTurns(long gameId, int numOfPlayers) {
+        return turnRepository.countByGameId(gameId) - numOfPlayers;
     }
 }

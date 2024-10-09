9fcfe7bb93abee88fceba8fa16130a691e8cf0d7
fdifrison <ing.giovanni.frison@gmail.com>
2024-10-09 16:31:11 +0200

    Fixed cors for delete

--- src/main/java/com/fdifrison/catan/core/configuration/CorsConfiguration.java
+++ src/main/java/com/fdifrison/catan/core/configuration/CorsConfiguration.java
@@ -11,6 +11,9 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }

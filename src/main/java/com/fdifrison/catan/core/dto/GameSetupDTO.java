a6c05d54c74353f2e6f8706c93a1e0b80c0e75f8
Giovanni Frison <ing.giovanni.frison@gmail.com>
2024-11-04 23:20:49 +0100

    Update GameSetupDTO.java

--- src/main/java/com/fdifrison/catan/core/dto/GameSetupDTO.java
+++ src/main/java/com/fdifrison/catan/core/dto/GameSetupDTO.java
@@ -11,11 +11,11 @@ import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 @Schema(description = "Required info to setup a new game")
-public record GameSetupDTO(@NotNull GameSetupDTO.SetupDTO gameInfo, @Min(2) List<GamePlayerInfoDTO> playersInfo) {
+public record GameSetupDTO(@NotNull GameSetupDTO.SetupDTO gameInfo, List<GamePlayerInfoDTO> playersInfo) {
 
     @Schema(description = "Basic info about the game")
     public record SetupDTO(
-            @NotNull String gameName, @NotNull Game.GameType gameType, @Min(8) int requiredVictoryPoints) {}
+            @NotNull String gameName, @NotNull Game.GameType gameType, int requiredVictoryPoints) {}
 
     @Schema(description = "Basic info about the players participating to the game")
     public record GamePlayerInfoDTO(
